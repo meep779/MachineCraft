9ddc74142c254ae983f5dffca7c7942562afa0f9
superguy9 <superguy2468@gmail.com>
2013-12-18 08:51:51 -0800

    Added Icon and Unlocalized Name

--- java/net/superguy9/machinecraft/items/ItemWrench.java
+++ java/net/superguy9/machinecraft/items/ItemWrench.java
@@ -1,7 +1,10 @@
 package net.superguy9.machinecraft.items;
 
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.Item;
 import net.superguy9.machinecraft.creativetabs.MachineCraftTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemWrench extends Item {
 	
@@ -9,5 +12,12 @@ public class ItemWrench extends Item {
 		super(id);
 		setMaxStackSize(1);
 		setCreativeTab(MachineCraftTabs.tabMachineCraftItems);
+		setUnlocalizedName(ItemInfo.WRENCH_UNLOCALIZED_NAME);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister register) {
+		itemIcon = register.registerIcon(ItemInfo.TEXTURE_LOCATION + ":" + ItemInfo.WRENCH_ICON);
 	}
 }
