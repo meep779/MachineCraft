b602b8d4a109da85790743a20f3a47cc664d3e3b
superguy9 <superguy2468@gmail.com>
2013-12-17 21:02:42 -0800

    Added a Couple Values
    Added setMaxStackSize and setCreativeTab values

--- java/net/superguy9/machinecraft/items/ItemWrench.java
+++ java/net/superguy9/machinecraft/items/ItemWrench.java
@@ -1,10 +1,13 @@
 package net.superguy9.machinecraft.items;
 
 import net.minecraft.item.Item;
+import net.superguy9.machinecraft.creativetabs.MachineCraftTabs;
 
 public class ItemWrench extends Item {
 	
 	public ItemWrench(int id){
 		super(id);
+		setMaxStackSize(1);
+		setCreativeTab(MachineCraftTabs.tabMachineCraftItems);
 	}
 }
