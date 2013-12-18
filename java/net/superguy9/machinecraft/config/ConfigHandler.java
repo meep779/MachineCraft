00245905c1df52d908e8dd342aa022dd2a1f8106
superguy9 <superguy2468@gmail.com>
2013-12-18 08:51:09 -0800

    Added ItemID Values

--- java/net/superguy9/machinecraft/config/ConfigHandler.java
+++ java/net/superguy9/machinecraft/config/ConfigHandler.java
@@ -3,15 +3,18 @@ package net.superguy9.machinecraft.config;
 import java.io.File;
 
 import net.minecraftforge.common.Configuration;
+import net.superguy9.machinecraft.items.ItemInfo;
 
 public class ConfigHandler {
 
-	private static final String CATEGORY_OPTIONS = "Options";
+	private static final String CATEGORY_ITEMS = "Items";
 	
 	public static void init(File file) {
 		Configuration config = new Configuration(file);
 		
 		config.load();
+		
+		ItemInfo.WRENCH_ID = config.getItem(CATEGORY_ITEMS, ItemInfo.WRENCH_KEY, ItemInfo.WRENCH_DEFAULT).getInt() - 256;
 		
 		config.save();
 	}
